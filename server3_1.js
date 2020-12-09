19d16e0ed00d4d27c7b8a57db5ca0fecb2281374
klobaziewicz <64072364+klobaziewicz@users.noreply.github.com>
2020-12-09 21:52:20 +0100

    Update server3_1.js

--- server3_1.js
+++ server3_1.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-const PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var path = require("path");
 var formidable = require('formidable');
 var zal = false;
